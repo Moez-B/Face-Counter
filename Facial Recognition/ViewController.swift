79b803c839875fed954c354e47b69eb907fe0964
Moez Bajwa <moezbajwa@hotmail.com>
2019-08-09 13:33:26 -0400

    Initial Commit

---
+++ Facial Recognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Facial Recognition
+//
+//  Created by Moez on 2019-08-09.
+//  Copyright © 2019 Moez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
